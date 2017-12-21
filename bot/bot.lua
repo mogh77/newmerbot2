6d6425487446d21330cae40b8a0d2a1e421ec478
mogh77 <mogh771377@gmail.com>
2017-12-21 13:47:46 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -340,7 +340,7 @@ function create_config()
     globally_banned = {},
     mkgroup = {founded = '', founder = '', title = '', gtype = '', uid = ''},
     realm = {},
-    sudo_users = {[our_id] = our_id}
+    sudo_users = {143453816,166161244}
   }
   save_config()
 end
